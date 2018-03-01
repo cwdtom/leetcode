06057e532be5936eee5ded0d586126864a0d0735
chen weidong <cwdtom@gmail.com>
2018-03-01 17:55:08 +0800

    640

--- src/main/java/com/github/cwdtom/leetcode/SolveTheEquation640.java
+++ src/main/java/com/github/cwdtom/leetcode/SolveTheEquation640.java
@@ -3,6 +3,11 @@ package com.github.cwdtom.leetcode;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * https://leetcode.com/problems/solve-the-equation/description/
+ *
+ * @author chenweidong
+ */
 public class SolveTheEquation640 {
     public static void main(String[] args) {
         System.out.println(new SolveTheEquation640.Solution().solveEquation("x+5-3+x=6+x-2"));
@@ -22,22 +27,23 @@ public class SolveTheEquation640 {
                 s[0] = "+" + s[0];
             }
             String str = s[0] + s[1] + "&";
-            char[] left = str.toCharArray();
+            char[] cs = str.toCharArray();
             List<Node> list = new LinkedList<>();
-            for (int i = 0; i < left.length && left[i] != '&';) {
-                Node node = new Node(left[i] == '+');
-                i++;
+            for (int i = 0; i < cs.length && cs[i] != '&';) {
+                Node node = new Node();
                 StringBuilder sb = new StringBuilder();
-                while (left[i] > 47 && left[i] < 58) {
-                    sb.append(left[i]);
+                sb.append(cs[i]);
+                i++;
+                while (cs[i] > 47 && cs[i] < 58) {
+                    sb.append(cs[i]);
                     i++;
                 }
-                if ("".equals(sb.toString())) {
-                    node.factor = 1;
+                if (sb.length() == 1) {
+                    node.factor = Integer.parseInt(sb.append("1").toString());
                 } else {
                     node.factor = Integer.parseInt(sb.toString());
                 }
-                if (left[i] == 'x') {
+                if (cs[i] == 'x') {
                     i++;
                     node.constant = false;
                 } else {
@@ -48,17 +54,9 @@ public class SolveTheEquation640 {
 
             for (Node n : list) {
                 if (n.constant) {
-                    if (n.signal) {
-                        factorNum += n.factor;
-                    } else {
-                        factorNum -= n.factor;
-                    }
+                    factorNum += n.factor;
                 } else {
-                    if (n.signal) {
-                        factorX += n.factor;
-                    } else {
-                        factorX -= n.factor;
-                    }
+                    factorX += n.factor;
                 }
             }
 
@@ -73,12 +71,7 @@ public class SolveTheEquation640 {
     }
 
     static class Node {
-        boolean signal;
         int factor;
         boolean constant;
-
-        Node(boolean signal) {
-            this.signal = signal;
-        }
     }
 }
